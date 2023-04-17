5af24c9450a7f136366473d597ccea93b686f6b7
Cristóbal Moraga <72986853+crismoraga@users.noreply.github.com>
2023-04-17 18:13:01 -0400

    PIR listo

--- Stage3/src/PIR_Detector.java
+++ Stage3/src/PIR_Detector.java
@@ -10,23 +10,6 @@ public class PIR_Detector extends Sensor{
         id = nextId++;
     }
 
-    public void inArea(Person p) { //booleano? (si es así, puede llamar otra función que obtenga el ángulo y distancia donde está la persona)
-        //Que reconozca que esta dentro del area
-        /*
-        if(detectar_persona() == True){ //pseudocódigo
-            int X = p.getXx();
-            int Y = p.getYy();
-            System.out.println("posición de la persona: ", X, Y);
-
-            //podemos usar estas coordenadas para hacer una función que calcule ángulos no?
-            //como un triángulo rectángulo, con medidas x,y entonces como tangente(alfa)=cateto opuesto/cateto adyacente podemos usar y=cateto opuesto
-            //y cateto adyacente = x entonces quedaría alfa = arctan(y/x)
-
-            float alfa = arctan(p.getYy/p.getXx);
-            System.out.println("ángulo de la persona: ", alfa, "°"); 
-        }
-        */
-    }
     @Override
     public State getState() {
         return state;
